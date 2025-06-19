134dfeaaf7ba452b89483c36e40eb91db76dfc11
Saannan <sanjayaadiputra376@gmail.com>
2025-06-19 20:33:42 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC9LImjPAa_kn028yEjVyyfHP_4Rb9y_CI",
+    authDomain: "vioo-code.firebaseapp.com",
+    databaseURL: "https://vioo-code-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vioo-code",
+    storageBucket: "vioo-code.firebasestorage.app",
+    messagingSenderId: "382046884335",
+    appId: "1:382046884335:web:4b136306938bdefabc661a",
+    measurementId: "G-JNYQCDV3CD"
+};
+
+export const supabaseConfig = {
+    url: 'https://fllyfxfiwajcmkqpvabz.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbHlmeGZpd2FqY21rcXB2YWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjA4NzMsImV4cCI6MjA2NTgzNjg3M30.fxav0iq1OX8iQ4IzD0pnGtOTb73E9yCf6-9v6d_Wb78',
+    bucket: 'vioo-code' 
+};
