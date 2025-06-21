5e5700c3046c32810016b21f5cc01eb9c28d9ce0
Saannan <sanjayaadiputra376@gmail.com>
2025-06-21 18:47:17 +0700

    Delete config directory

--- config/supabase-init.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { supabaseConfig } from './config.js';
-
-export const supabase = createClient(supabaseConfig.url, supabaseConfig.anonKey);
